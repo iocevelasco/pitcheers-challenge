1365466d1a7aa116cb7d37450c07a2f07ebe5275
iocevelasco <iocevelasco@gmail.com>
2022-03-29 10:57:29 -0300

    feat: add few test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import {
+  render,
+  waitForElement,
+  fireEvent,
+  screen,
+} from '@testing-library/react'
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('render tenants list', async () => {
+  render(<App />);
+
+  expect(screen.getByText('Loading...')).toBeTruthy()
+  await waitForElement(() => {
+    expect(screen.getAllByTestId('tenant-row'))
+  }
+    , { timeout: 4000 })
 });
+test('Should show form after click button', async () => {
+  render(<App />);
+  fireEvent.click(screen.getByText('Add Tenant'))
+  expect(screen.getByTestId('form'))
+});
+
+test('Should filter by tabs', async () => {
+  render(<App />);
+  await waitForElement(() => {
+    fireEvent.click(screen.getByTestId('tab-0'))
+    expect(screen.getByTestId('tenant-row'))
+  }
+    , { timeout: 4000 })
+}); 
